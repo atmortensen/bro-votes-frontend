a60a6dfba4d9f7179fbd4f78b1107383f948b778
Alex Mortensen <alextmortensen@gmail.com>
2019-09-21 13:15:36 -0600

    ios bro

--- src/helpers/location.helper.js
+++ src/helpers/location.helper.js
@@ -5,24 +5,28 @@ export function useBroLocation(props) {
   const { broLocation, setBroLocation } = useContext(BroLocationContext);
 
   useEffect(() => {
-    if (!navigator || !navigator.geolocation) {
+    if (!window.navigator || !window.navigator.geolocation) {
       return alert('Your browser is not supported, sorry bro!');
     }
     if (!broLocation) {
-      navigator.geolocation.getCurrentPosition(position => {
+      window.navigator.geolocation.getCurrentPosition(position => {
         setBroLocation({
           long: position.coords.longitude,
           lat: position.coords.latitude
         });
       });
 
-      navigator.permissions.query({ name: 'geolocation' }).then(res => {
-        if (res.state === 'denied') {
-          return alert(
-            `Bro, you've disabled geolocation tracking. Enable it bro.`
-          );
-        }
-      });
+      if (window.navigator.permissions.query) {
+        window.navigator.permissions
+          .query({ name: 'geolocation' })
+          .then(res => {
+            if (res.state === 'denied') {
+              return alert(
+                `Bro, you've disabled geolocation tracking. Enable it bro.`
+              );
+            }
+          });
+      }
     }
   });
 
